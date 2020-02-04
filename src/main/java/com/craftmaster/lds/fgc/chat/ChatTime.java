af1c37cfa8f205dbca813f1997c54b49fc5ac6a6
Carson <mr.carson.fisher@gmail.com>
2020-02-03 22:59:46 -0700

    Put date in time stamp.

--- src/main/java/com/craftmaster/lds/fgc/chat/ChatTime.java
+++ src/main/java/com/craftmaster/lds/fgc/chat/ChatTime.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class ChatTime {
 	public String getCurrentTime(){
 			// Returns formatted current time as a string.
-		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a");  
+		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd h:mm:ss a");  
 		   LocalDateTime now = LocalDateTime.now();  
 		   //System.out.println(dtf.format(now)); 
 		   return dtf.format(now);
