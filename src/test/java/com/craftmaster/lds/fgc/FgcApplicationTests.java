ea221dac67fe20eea11687fb2b4a0f2285e44d2c
Bryce J. Fisher <bryce.fisher@gmail.com>
2020-03-06 15:39:16 -0700

    Fixed broken gradle test.

--- src/test/java/com/craftmaster/lds/fgc/FgcApplicationTests.java
+++ src/test/java/com/craftmaster/lds/fgc/FgcApplicationTests.java
@@ -1,5 +1,8 @@
 package com.craftmaster.lds.fgc;
 
+import com.opentable.db.postgres.junit.EmbeddedPostgresRules;
+import com.opentable.db.postgres.junit.SingleInstancePostgresRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FgcApplicationTests {
+
+  @Rule public SingleInstancePostgresRule pg = EmbeddedPostgresRules.singleInstance();
 
   @Test
   public void contextLoads() {}
