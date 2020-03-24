85314c47f69b16a408fe95aeeca1c1d78366271f
Joe Fisher <javajoe@churchofjesuschrist.org>
2020-03-24 11:39:39 -0600

    #61 disable scheduling during unit test

--- server/src/main/java/com/craftmaster/lds/fgc/FgcApplication.java
+++ server/src/main/java/com/craftmaster/lds/fgc/FgcApplication.java
@@ -2,10 +2,12 @@ package com.craftmaster.lds.fgc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@ConditionalOnProperty(value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true)
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class FgcApplication {
--- server/src/test/java/com/craftmaster/lds/fgc/FgcApplicationTests.java
+++ server/src/test/java/com/craftmaster/lds/fgc/FgcApplicationTests.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = "app.scheduling.enable=false")
 public class FgcApplicationTests {
 
   @Test
