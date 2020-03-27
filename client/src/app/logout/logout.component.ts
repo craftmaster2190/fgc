a6372741c5df72d2485c497aca6ca24d9959acaa
Bryce J. Fisher <bryce.fisher@gmail.com>
2020-03-27 12:13:40 -0600

    Adding basic CSRF protection.
    fixed #46

--- client/src/app/logout/logout.component.ts
+++ client/src/app/logout/logout.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { DeviceUsersService } from "../auth/device-users.service";
+import timeout from "../util/timeout";
 
 @Component({
   selector: "app-logout",
   templateUrl: "./logout.component.html",
   styleUrls: ["./logout.component.scss"]
 })
-export class LogoutComponent {}
+export class LogoutComponent implements OnInit {
+  showHelper = false;
+  ngOnInit(): void {
+    timeout(5000).then(() => (this.showHelper = true));
+  }
+}
